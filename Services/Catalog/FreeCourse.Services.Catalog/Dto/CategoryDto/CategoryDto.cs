fce6fcc1030620f763bec6184970b7d04f4d4a16
Semih DİZDAR <semih.dizdar@bil.omu.edu.tr>
2022-01-13 01:05:48 +0300

    Add project files.

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Dto/CategoryDto/CategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace FreeCourse.Services.Catalog.Dto.CategoryDto
+{
+    public class CategoryDto
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Services/Catalog/FreeCourse.Services.Catalog/Dto/FeatureDto/FeatureDto.cs
@@ -0,0 +1,7 @@
+﻿namespace FreeCourse.Services.Catalog
+{
+    public class FeatureDto
+    {
+        public int Duration { get; set; }
+    }
+}
